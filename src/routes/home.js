c5469d940d6a1061570630413db2644dc33c3edf
Usman Tahir <ch.usman.tahir@gmail.com>
2020-06-15 04:17:40 +0500

    fixed route controller

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get((req, res) => {
+router.get("/", (req, res) => {
   return res.json({
     message: "Welcome API to Home",
     author: "Usman Tahir",
