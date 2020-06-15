e0eb59a3a7f041b4253162647c6b85e06653ecbe
Usman Tahir <ch.usman.tahir@gmail.com>
2020-06-15 05:48:24 +0500

    added web scraper along with pagination

--- src/routes/tretton.js
+++ src/routes/tretton.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+const getNinjas = require("../controllers/tretton.getNinjas");
+
 router.get("/", (req, res) => {
   return res.json({
     message: "Welcome tretton37 API to Home",
     author: "Usman Tahir",
   });
 });
+
+router.get("/ninjas", getNinjas);
 
 module.exports = router;
