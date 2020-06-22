c3bde8753fc8d81f912e3ae148a1dfd866634861
Usman Tahir <ch.usman.tahir@gmail.com>
2020-06-22 06:00:24 +0500

    fixed integer parsing issue on page numbers and length of response

--- src/controllers/tretton.getNinjas.js
+++ src/controllers/tretton.getNinjas.js
@@ -7,14 +7,22 @@ const scraper = new Scraper("https://api.tretton37.com/ninjas", true);
 async function getNinjas(req, res) {
   let { pageNumber, pageSize } = req.query;
 
-  if (!pageNumber) pageNumber = 0;
-  if (!pageSize) pageSize = 10;
+  if (!pageNumber) {
+    pageNumber = 0;
+  } else {
+    pageNumber = parseInt(pageNumber);
+  }
+  if (!pageSize) {
+    pageSize = 10;
+  } else {
+    pageSize = parseInt(pageSize);
+  }
 
   const startIndex = pageNumber * pageSize;
   const endIndex = startIndex + pageSize;
   const prepareResponse = () => ({
-    pageNumber: parseInt(pageNumber),
-    pageSize: parseInt(pageSize),
+    pageNumber,
+    pageSize,
     length: allNinjas.length,
     data: allNinjas.slice(startIndex, endIndex),
   });
